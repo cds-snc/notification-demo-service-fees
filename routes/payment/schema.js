f78e46700913b3af918c03cf8f40a045eaac97ff
Tim Arney <tim@line37.com>
2019-09-04 11:28:48 -0400

    add credit card #

--- routes/payment/schema.js
+++ routes/payment/schema.js
@@ -1,8 +1,18 @@
+const isCreditCard = require("validator/lib/isCreditCard").default;
+
 const Schema = {
   card_type: {
     isIn: {
       errorMessage: "errors.card_type.valid",
       options: [["Visa", "MasterCard"]]
+    }
+  },
+  card_number: {
+    custom: {
+      options: (value, { req }) => {
+        return isCreditCard(value);
+      },
+      errorMessage: "errors.card_number.valid"
     }
   },
   ccv: {
